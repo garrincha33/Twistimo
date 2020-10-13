e3a2b451ec0c5d37af4233cdc0037fd9135e7344
Richard Price <mrspondooli@gmail.com>
2020-10-13 18:40:50 +0100

    initial commit

---
+++ Twisitimo/Twisitimo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twisitimo
+//
+//  Created by Richard Price on 13/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twisitimo/Twisitimo/TwisitimoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwisitimoApp.swift
+//  Twisitimo
+//
+//  Created by Richard Price on 13/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TwisitimoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
